33a561f5c0b334b230c8e449835c77150d3e4c77
Michael Roose <mjroose@gmail.com>
2018-08-13 19:28:10 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,13 @@
 class Song
+  attr_reader :title, :artist, :genre
   @@all = []
+
+  def initialize(title, artist, genre)
+    @title = title
+    @artist = artist
+    @genre = genre
+    self.class.all << self
+  end
 
   def self.all
     @@all
