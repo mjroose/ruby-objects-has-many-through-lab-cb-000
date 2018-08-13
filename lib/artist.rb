b27613cb129fad8f8a7054cb476694e2aa9f25bc
Michael Roose <mjroose@gmail.com>
2018-08-13 19:26:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,4 +10,16 @@ class Artist
   def self.all
     @@all
   end
+
+  def new_song(title, genre) #add a new song and make song aware of its artist
+
+  end
+
+  def songs #use select to return a list of all of the artist's songs
+
+  end
+
+  def genres #returns a list of the genre of each song
+
+  end
 end
